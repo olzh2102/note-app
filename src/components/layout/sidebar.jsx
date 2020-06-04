e9dc2fcd9430cb8584e1e0f9331bf167e9a1247e
Olzhas Kurikov <olzhas2102@yahoo.com>
2020-06-04 21:06:01 +0200

    continuing with sidebar component

--- src/components/layout/sidebar.jsx
+++ src/components/layout/sidebar.jsx
@@ -1,7 +1,45 @@
 import React from 'react'
+import {
+	FaChevronDown,
+	FaInbox,
+	FaRegCalendarAlt,
+	FaRegCalendar,
+} from 'react-icons/fa'
 
 const Sidebar = () => {
-	return <p>I am a sidebar component</p>
+	return (
+		<div className="sidebar" data-testid="sidebar">
+			<ul className="sidebar__generic">
+				<li>
+					<span>
+						<FaInbox />
+					</span>
+					<span>Inbox</span>
+				</li>
+				<li>
+					<span>
+						<FaRegCalendar />
+					</span>
+					<span>Calendar</span>
+				</li>
+				<li>
+					<span>
+						<FaRegCalendarAlt />
+					</span>
+					<span>Next 7 Days</span>
+				</li>
+			</ul>
+
+			<div className="sidebar__middle">
+				<span>
+					<FaChevronDown />
+				</span>
+				<h2>Projects</h2>
+			</div>
+
+			<ul className="sidebar__projects">Projects will go here</ul>
+		</div>
+	)
 }
 
 export default Sidebar
